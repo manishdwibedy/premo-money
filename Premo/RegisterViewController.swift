dd7ec39ced308135f64677be22d50a3210bee031
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-17 13:26:03 -0700

    Adding actions and outlets.

---
+++ Premo/RegisterViewController.swift
@@ -0,0 +1,40 @@
+//
+//  RegisterViewController.swift
+//  Premo
+//
+//  Created by Manish Dwibedy on 6/17/16.
+//  Copyright © 2016 Manish Dwibedy. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var fullName: UITextField!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func register(sender: AnyObject) {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
