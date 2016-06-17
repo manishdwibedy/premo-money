fef209607af064a72714834446a18e87e543d7e3
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-17 12:25:57 -0700

    Now, using custom button to do Facebook login.

--- Premo/ViewController.swift
+++ Premo/ViewController.swift
@@ -12,15 +12,36 @@ import FBSDKLoginKit
 class ViewController: UIViewController {
 
     @IBAction func facebookLogin(sender: AnyObject) {
-        
+        var fbLoginManager : FBSDKLoginManager = FBSDKLoginManager()
+        fbLoginManager .logInWithReadPermissions(["email"], handler: { (result, error) -> Void in
+            if (error == nil){
+                var fbloginresult : FBSDKLoginManagerLoginResult = result
+                if(fbloginresult.grantedPermissions.contains("email"))
+                {
+                    self.getFBUserData()
+                    fbLoginManager.logOut()
+                }
+            }
+        })
     }
+    
+    func getFBUserData(){
+        if((FBSDKAccessToken.currentAccessToken()) != nil){
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name, picture.type(large), email"]).startWithCompletionHandler({ (connection, result, error) -> Void in
+                if (error == nil){
+                    print(result)
+                }
+            })
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let loginButton = FBSDKLoginButton()
-        loginButton.center = view.center
-        view.addSubview(loginButton)
+//        let loginButton = FBSDKLoginButton()
+//        loginButton.center = view.center
+//        view.addSubview(loginButton)
     }
 
     override func didReceiveMemoryWarning() {
