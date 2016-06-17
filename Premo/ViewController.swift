90b15e411e13bab350a0f646760f95545d07b161
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-17 12:07:28 -0700

    Adding an action for Facebook login.

--- Premo/ViewController.swift
+++ Premo/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func facebookLogin(sender: AnyObject) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
