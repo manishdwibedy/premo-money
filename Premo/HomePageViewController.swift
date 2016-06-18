c7cf039f7a11267a56eb7c747b72aaec02c09dc1
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-17 18:33:25 -0700

    Adding the home page for the app.

---
+++ Premo/HomePageViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HomePageViewController.swift
+//  Premo
+//
+//  Created by Manish Dwibedy on 6/17/16.
+//  Copyright © 2016 Manish Dwibedy. All rights reserved.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
