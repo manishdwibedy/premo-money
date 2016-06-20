7876ca0da9ec9d256fbacd95a20a9bd2d8175dc7
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-20 14:49:05 -0700

    Creating the UI for the settings screen. Also linked the controls to the view controller.

--- Premo/SettingsViewController.swift
+++ Premo/SettingsViewController.swift
@@ -8,15 +8,32 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var dob: UITextField!
+    @IBOutlet weak var address: UITextView!
+    @IBOutlet weak var datepicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
+        dob.delegate = self
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool{
+        // Would be fixing the bug, soon
+        // datepicker.alpha = 1
+        return true
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        address.setContentOffset(CGPointZero, animated: false)
     }
 }
