eacd5b248fe7792dc360dc677aa77d2ff386357d
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-20 13:55:39 -0700

    Added comments.

--- Premo/utility.swift
+++ Premo/utility.swift
@@ -11,6 +11,12 @@ import UIKit
 import CryptoSwift
 
 class Util{
+    
+    /*!
+     * @discussion Checking the password for its strength. If the password contains a capital alphabet, a number and a special character, then the method returns true, else it would return false
+     * @param password string
+     * @return Return true if the password is strong.
+     */
     static func checkPasswordStrength(password: String) -> Bool{
         let capitalTest = NSPredicate(format:"SELF MATCHES %@", ".*[A-Z]+.*")
         let numberTest = NSPredicate(format:"SELF MATCHES %@", ".*[0-9]+.*")
@@ -24,6 +30,11 @@ class Util{
         return capitalresult && numberresult && specialresult
     }
 
+    /*!
+     * @discussion This method would hash the string using SHA 512
+     * @param string data
+     * @return hashed string representation of the data
+     */
     static func hash(data: String) -> String {
         let bytes = data.utf8.map({$0})
         let hash = bytes.sha512()
@@ -31,6 +42,11 @@ class Util{
         return hash.toHexString()
     }
     
+    /*!
+     * @discussion This method would hash the salted version of the string
+     * @param strign data
+     * @return salted version of the data
+     */
     static func salt(data: String) -> String{
         let hashed = Util.hash(data)
         let salt = "YQeF7wSdbk1Vbfp79umh"
@@ -39,11 +55,20 @@ class Util{
         return saltedPassword
     }
     
+    /*!
+     * @discussion Gets the vender identifier to be used as a ID
+     * @param <#param description#>
+     * @return <#return description#>
+     */
     static func getDeviceID() -> String{
         return UIDevice.currentDevice().identifierForVendor!.UUIDString
     }
     
-    // Checking where the input string is a valid email or not
+    /*!
+     * @discussion Checks where the string is a valid email or not.
+     * @param strign data
+     * @return returns true if the string is a valid email, else returns false.
+     */
     static func isValidEmail(inputString:String) -> Bool {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
         let range = inputString.rangeOfString(emailRegEx, options:.RegularExpressionSearch)
