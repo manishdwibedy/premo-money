f110455a66cdff934acf003c7d6616900fe97a7e
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-20 18:45:04 -0700

    Added the donation picker.

--- Premo/HostingPartyViewController.swift
+++ Premo/HostingPartyViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class HostingPartyViewController: UIViewController {
+class HostingPartyViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     @IBOutlet weak var maxCapacity: UILabel!
+    @IBOutlet weak var donationPicker: UIPickerView!
+    
+    var pickerData: [String] = [String]()
+    
     @IBAction func valueChanged(sender: UIStepper) {
         maxCapacity.text = Int(sender.value).description
     }
@@ -17,10 +21,29 @@ class HostingPartyViewController: UIViewController {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
+        pickerData = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"]
+        
+        self.donationPicker.delegate = self
+        self.donationPicker.dataSource = self
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // The number of columns of data
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
     }
 }
