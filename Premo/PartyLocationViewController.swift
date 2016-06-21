340b4fdf25ec3656c4a3156e2a825c2cc502b8c8
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-20 18:13:38 -0700

    Added the party location UI, added a segmented control for filtering and the map is attached.

---
+++ Premo/PartyLocationViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PartyLocationViewController.swift
+//  Premo
+//
+//  Created by Manish Dwibedy on 6/20/16.
+//  Copyright © 2016 Manish Dwibedy. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class PartyLocationViewController: UIViewController {
+    @IBOutlet weak var map: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
