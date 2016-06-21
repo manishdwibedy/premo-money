ac29a0122839c94b02f4763dc879a4eacccd13c4
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-21 14:27:26 -0700

    Linking the settings data in the DB to the text fields.

--- Premo/SettingsViewController.swift
+++ Premo/SettingsViewController.swift
@@ -16,10 +16,27 @@ class SettingsViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var address: UITextView!
     @IBOutlet weak var datepicker: UIDatePicker!
     
+    let db_ref = FIRDatabase.database().reference()
+    let uid = FIRAuth.auth()?.currentUser?.uid
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let settingRef = db_ref.child("user_info")
+
+        let _ = settingRef.observeEventType(FIRDataEventType.Value, withBlock: { (snapshot) in
+            let data = snapshot.value as! [String : AnyObject]
+            
+            let user_data = data[self.uid!]!
+            
+            self.dob.text = user_data["dob"]!! as! String
+            self.address.text = user_data["address"]!! as! String
+            self.username.text = user_data["username"]!! as! String
+            // ...
+        })
+        
         dob.delegate = self
         username.text = "manish.dwibedy"
         dob.text = "April 17, 1989"
@@ -55,10 +72,6 @@ class SettingsViewController: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func saveSettings(sender: UIBarButtonItem) {
-        let uid = FIRAuth.auth()?.currentUser?.uid
-        
-        let db_ref = FIRDatabase.database().reference()
-        
         let user_data = [
             "username": username.text!,
             "dob" : dob.text!,
