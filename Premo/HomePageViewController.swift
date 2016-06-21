c32090b8d7a3da8aabb0cdcf73c28f23082d7b3a
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-20 17:14:08 -0700

    Handled the filter of parties.

--- Premo/HomePageViewController.swift
+++ Premo/HomePageViewController.swift
@@ -9,11 +9,47 @@
 import UIKit
 
 class HomePageViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var parties = [[String:String]]()
+    var filteredRows = [[String:String]]()
+    let partyCount = 10
+    let types = ["I", "H", "S"]
+    
+    @IBAction func typeChanged(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex
+        {
+            case 0:
+                self.getFilteredContent("I")
+            case 1:
+                self.getFilteredContent("S")
+            case 2:
+                self.getFilteredContent("H")
+            default:
+                break
+        }
+    }
+    
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for index in Array(1...self.partyCount){
+            let type = Int(arc4random_uniform(3))
+
+            let party: [String:String] = [
+                "name" : "Party No. \(index)",
+                "description" : "Party Description for Party No. \(index)",
+                "image" : "image URL",
+                "type": self.types[type]
+            ]
+            parties.append(party)
+        }
+        filteredRows = parties
+        
+        // By default, get Indigo
+        getFilteredContent("I")
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -28,7 +64,7 @@ class HomePageViewController: UIViewController, UITableViewDataSource, UITableVi
     
     // Number of rows
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return filteredRows.count
     }
     
     // Rendering individual cell
@@ -37,8 +73,9 @@ class HomePageViewController: UIViewController, UITableViewDataSource, UITableVi
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
         
         let row = indexPath.row
-        cell.textLabel?.text = "Text \(row)"
-        cell.detailTextLabel?.text = "Detail \(row)"
+        let party = filteredRows[row]
+        cell.textLabel?.text = party["name"]
+        cell.detailTextLabel?.text = party["description"]
         return cell
     }
     
@@ -47,6 +84,19 @@ class HomePageViewController: UIViewController, UITableViewDataSource, UITableVi
         let row = indexPath.row
         
         print("Selected the speaker :\(row)")
+    }
+    
+    func getFilteredContent(type: String){
+        var rows = [[String:String]]()
+        
+        for row in self.parties{
+            if row["type"] == type{
+                rows.append(row)
+            }
+        }
+        self.filteredRows = rows
+        tableView.reloadData()
+        
     }
 
 }
