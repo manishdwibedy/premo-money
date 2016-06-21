733488275d64ffacbeca0d95ccb058c765d27bb9
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-21 15:06:20 -0700

    Now, the map focusses on Los Angeles.

--- Premo/PartyLocationViewController.swift
+++ Premo/PartyLocationViewController.swift
@@ -14,11 +14,23 @@ class PartyLocationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view, typically from a nib.        
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let initialLocation = CLLocation(latitude: 34.051454, longitude: -118.249029)
+        centerMapOnLocation(initialLocation)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    let regionRadius: CLLocationDistance = 5000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 2.0, regionRadius * 2.0)
+        map.setRegion(coordinateRegion, animated: true)
+        
+        
     }
 }
