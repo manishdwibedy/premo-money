893d1e12c4c1904e2f1bf5d14f1d11f6db97f56f
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-23 13:14:06 -0700

    Adding host, capacity, timestamp and guests in the party object.

--- Premo/Party.swift
+++ Premo/Party.swift
@@ -14,11 +14,23 @@ class Party: NSObject, MKAnnotation {
     var coordinate: CLLocationCoordinate2D
     var info: String
     var type: String
+    var host: String
+    var capacity: Int
+    var timestamp: String
+    var guests = [String]()
     
-    init(title: String, coordinate: CLLocationCoordinate2D, info: String, type: String) {
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String, type: String, host: String, capacity: Int, timestamp: String) {
         self.title = title
         self.coordinate = coordinate
         self.info = info
         self.type = type
+        self.host = host
+        self.capacity = capacity
+        self.timestamp = timestamp
+    }
+    
+    func addGuest(guestID: String) -> Int{
+        guests.append(guestID)
+        return guests.count
     }
 }
