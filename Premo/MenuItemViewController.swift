0c4795122224ed4029ebd856ab10d6a4d97ded3c
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-23 16:18:13 -0700

    Connected the controls.

--- Premo/MenuItemViewController.swift
+++ Premo/MenuItemViewController.swift
@@ -9,11 +9,18 @@
 import UIKit
 
 class MenuItemViewController: UIViewController {
+    @IBOutlet weak var party_type: UISegmentedControl!
+    @IBOutlet weak var menu_desc: UITextView!
+    @IBOutlet weak var menu_item: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    @IBAction func addMenu(sender: AnyObject) {
+        print(party_type.selectedSegmentIndex)
     }
 }
