ca8f50b36242944fcc54d4170dc6f6dd56266e6c
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-23 16:13:16 -0700

    Adding view controller.

---
+++ Premo/MenuItemViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MenuItemViewController.swift
+//  Premo
+//
+//  Created by Manish Dwibedy on 6/23/16.
+//  Copyright © 2016 Manish Dwibedy. All rights reserved.
+//
+
+import UIKit
+
+class MenuItemViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
