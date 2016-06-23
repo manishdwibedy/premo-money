3032e901f7c2cf0f1b076003cea2c39d43d53ed9
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-23 16:45:09 -0700

    Saving the menu item to the DB.

--- Premo/MenuItemViewController.swift
+++ Premo/MenuItemViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import FirebaseAuth
 
 class MenuItemViewController: UIViewController {
     @IBOutlet weak var party_type: UISegmentedControl!
     @IBOutlet weak var menu_desc: UITextView!
     @IBOutlet weak var menu_item: UITextField!
+    
+    let db_ref = FIRDatabase.database().reference()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,5 +27,18 @@ class MenuItemViewController: UIViewController {
     }
     @IBAction func addMenu(sender: AnyObject) {
         print(party_type.selectedSegmentIndex)
+        
+        let type = Constant.party_type[party_type.selectedSegmentIndex]
+        let index = type.startIndex.advancedBy(1)
+        
+        let uid = FIRAuth.auth()?.currentUser?.uid
+        var menu_list = [[String:String]]()
+        let menu = [
+            "item": self.menu_item.text!,
+            "desc" : self.menu_desc.text!,
+            "type" : type.substringToIndex(index)
+        ]
+        menu_list.append(menu)
+        self.db_ref.child("user_info/\(uid!)/menu").setValue(menu_list)
     }
 }
