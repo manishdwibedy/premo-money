bf99469815bfa2f963b446eda2dcd2827af254f8
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-29 13:55:39 -0700

    Added the documentation for the party model class.

--- Premo/Party.swift
+++ Premo/Party.swift
@@ -9,14 +9,31 @@
 import MapKit
 import UIKit
 
+// The party model class
 class Party: NSObject, MKAnnotation {
+    
+    // Party's title
     var title: String?
+    
+    // Party's Co-ordinates
     var coordinate: CLLocationCoordinate2D
+    
+    // Party's Description
     var info: String
+    
+    // Party;s type
     var type: String
+    
+    // Party's Host
     var host: String
+    
+    // Party's Capacity
     var capacity: Int
+    
+    // Party's creation timestamp
     var timestamp: String
+    
+    // Party's guest list
     var guests = [String]()
     
     init(title: String, coordinate: CLLocationCoordinate2D, info: String, type: String, host: String, capacity: Int, timestamp: String) {
@@ -29,6 +46,11 @@ class Party: NSObject, MKAnnotation {
         self.timestamp = timestamp
     }
     
+    /*!
+     * @discussion Adding guest to the party's guest list
+     * @param Guest ID to be added
+     * @return the length of the guest list
+     */
     func addGuest(guestID: String) -> Int{
         guests.append(guestID)
         return guests.count
