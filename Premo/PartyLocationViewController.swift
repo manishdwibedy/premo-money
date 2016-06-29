5f8b2aada1c798ea2bddcce826de454dfa7d4fa6
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-29 15:53:37 -0700

    Adding documentation for Party Location screen.

--- Premo/PartyLocationViewController.swift
+++ Premo/PartyLocationViewController.swift
@@ -12,20 +12,35 @@ import CoreLocation
 import FirebaseDatabase
 import FirebaseAuth
 
+// This ViewController handles the party location screen
 class PartyLocationViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate, UITableViewDelegate, UITableViewDataSource  {
     @IBOutlet weak var map: MKMapView!
     var locationManager: CLLocationManager!
+    
+    // The list of parties
     var parties = [MKAnnotation]()
+    
+    // The list of filtered parties
     var filteredParties = [MKAnnotation]()
+    
+    // Holds the current party
     var selectedParty: Party?
+    
+    // Is a party selected
     var isPartySelected = false
+    
+    // User's Info
     var userInfo = [String: NSDictionary]()
+    
+    // The DB reference
+    let db_ref = FIRDatabase.database().reference()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
         
+        // Getting the user's location
         self.locationManager = CLLocationManager()
         self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
         self.locationManager.delegate = self;
@@ -33,11 +48,11 @@ class PartyLocationViewController: UIViewController, CLLocationManagerDelegate, 
         let initialLocation = CLLocation(latitude: 34.051454, longitude: -118.249029)
         centerMapOnLocation(initialLocation)
         
+        // present an alert indicating location authorization required
+        // and offer to take the user to Settings for the app via
+        // UIApplication -openUrl: and UIApplicationOpenSettingsURLString
         let status = CLLocationManager.authorizationStatus()
         if status == .NotDetermined || status == .Denied || status == .AuthorizedWhenInUse {
-            // present an alert indicating location authorization required
-            // and offer to take the user to Settings for the app via
-            // UIApplication -openUrl: and UIApplicationOpenSettingsURLString
             locationManager.requestAlwaysAuthorization()
             locationManager.requestWhenInUseAuthorization()
         }
@@ -46,11 +61,11 @@ class PartyLocationViewController: UIViewController, CLLocationManagerDelegate, 
         
         map.showsUserLocation = true
         
-        let db_ref = FIRDatabase.database().reference()
-        
         let settingRef = db_ref.child("party")
         
         var count = 1
+        
+        // Getting the party information
         let _ = settingRef.observeSingleEventOfType(FIRDataEventType.Value, withBlock: { (snapshot) in
             let party_data = snapshot.value as! [String : AnyObject]
             
@@ -80,11 +95,13 @@ class PartyLocationViewController: UIViewController, CLLocationManagerDelegate, 
             self.map.addAnnotations(self.parties)
             self.party_list.reloadData()
             
+            // Show the hybrid parties
             self.showParty("H")
             count += 1
             
         })
         
+        // Getting the user information
         let userRef = db_ref.child("user_info")
         let _ = userRef.observeSingleEventOfType(FIRDataEventType.Value, withBlock: { (snapshot) in
             let user_data = snapshot.value as! [String : AnyObject]
@@ -100,7 +117,6 @@ class PartyLocationViewController: UIViewController, CLLocationManagerDelegate, 
         })
 
         map.delegate = self
-        
         party_list.delegate = self
         party_list.dataSource = self
     }
@@ -111,12 +127,21 @@ class PartyLocationViewController: UIViewController, CLLocationManagerDelegate, 
     }
     
     let regionRadius: CLLocationDistance = 5000
+    /*!
+     * @discussion Centering the map to cover 5000 miles
+     * @param location - location of the map
+     */
     func centerMapOnLocation(location: CLLocation) {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
                                                                   regionRadius * 2.0, regionRadius * 2.0)
         map.setRegion(coordinateRegion, animated: true)
     }
     
+    /*!
+     * @discussion Location maanager to handle the location
+     * @param manager - location manager
+     * @param didUpdateLocations - locations
+     */
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations.last
         
@@ -126,6 +151,12 @@ class PartyLocationViewController: UIViewController, CLLocationManagerDelegate, 
 //        self.map.setRegion(region, animated: true)
     }
     
+    /*!
+     * @discussion Adding an annotation on the map
+     * @param lat - latitude
+     * @param long - longitude
+     * @param name - name of the annotaion
+     */
     func addAnnotationsOnMap(lat: Double, long: Double, name: String){
         // show the location on the map
         let location = CLLocationCoordinate2DMake(lat, long)
@@ -139,41 +170,47 @@ class PartyLocationViewController: UIViewController, CLLocationManagerDelegate, 
         map.addAnnotation(dropPin)
     }
     
+    /*!
+     * @discussion Rendering the mapview
+     * @param mapView - MKMapView
+     * @param viewForAnnotation - MKAnnotation
+     * @return nil
+     */
     func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
-        // 1
+        // ID for the annotation
         let identifier = "Party"
         
-        // 2
+        // Checking if the annotation is a party annotation
         if annotation is Party {
-            // 3
+            // Getting the annotation using the ID
             var annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier)
             
             let party = annotation as! Party
             
             if annotationView == nil {
-                //4
+                // Creating an annotation
                 annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
                 annotationView!.canShowCallout = true
                 annotationView!.image = UIImage(named:"party")
                 
-                // 5
+                // Creating the detail button
                 let btn = UIButton(type: .DetailDisclosure)
                 btn.tintColor = UIColor.purpleColor()
                 annotationView!.rightCalloutAccessoryView = btn
                 
+                // Adding the user icon on the annotation
                 let hostIcon = UIImageView(frame: CGRectMake(0, 0, 45, 45))
                 hostIcon.image=UIImage(named: "dummy-user.jpg")
                 annotationView?.leftCalloutAccessoryView = hostIcon
                 
+                // Adding the subtitle on the annotation
                 let subtitleView = UILabel()
                 subtitleView.font = subtitleView.font.fontWithSize(12)
                 subtitleView.numberOfLines = 0
                 subtitleView.text = party.info
                 annotationView!.detailCalloutAccessoryView = subtitleView
-                
-                
             } else {
-                // 6
+                // If the annotation is not nil, then use it
                 annotationView!.annotation = annotation
             }
             return annotationView
@@ -183,20 +220,28 @@ class PartyLocationViewController: UIViewController, CLLocationManagerDelegate, 
         return nil
     }
     
+    /*!
+     * @discussion Showing an alert box when the detail button is clicked
+     * @param mapView - MKMapView
+     * @param annotationView - MKAnnotationView
+     * @param calloutAccessoryControlTapped - UIControl
+     */
     func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
         let party = view.annotation as! Party
         let placeName = party.title
         let placeInfo = party.info
         
-        
+        // Adding an alert box
         let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .Alert)
         
-        // Create the actions
+        // Create the OK action
         let okAction = UIAlertAction(title: "OK", style: .Default, handler: { (action: UIAlertAction!) in
             self.isPartySelected = true
             self.selectedParty = party
             self.showPartyDescription()
         })
+        
+        // Create the cancel action
         let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: { (action: UIAlertAction!) in
             self.isPartySelected = false
         })
@@ -206,6 +251,11 @@ class PartyLocationViewController: UIViewController, CLLocationManagerDelegate, 
 
         presentViewController(ac, animated: true, completion: nil)
     }
+    
+    /*!
+     * @discussion Filtering the parties on the based of segmented control
+     * @param sender - UISegmentedControl
+     */
     @IBAction func typeChanged(sender: UISegmentedControl) {
         switch sender.selectedSegmentIndex {
             case 0:
@@ -220,12 +270,17 @@ class PartyLocationViewController: UIViewController, CLLocationManagerDelegate, 
 
     }
     
+    /*!
+     * @discussion Filtering the parties
+     * @param type - the party's type
+     */
     func showParty(type: String){
-        
+        // If showing the parties on the map
         if currentDisplayStyle == "map"{
             self.map.removeAnnotations(self.parties)
             var filteredAnnotations = [MKAnnotation]()
             
+            // For hybrid, show all parties
             if type == "H"{
                 filteredAnnotations = self.parties
             }
@@ -239,8 +294,12 @@ class PartyLocationViewController: UIViewController, CLLocationManagerDelegate, 
             }
             map.addAnnotations(filteredAnnotations)
         }
+            
+        // If showing the parties on the list
         else{
             self.filteredParties = [MKAnnotation]()
+            
+            // For hybrid, show all parties
             if type == "H"{
                 self.filteredParties = self.parties
             }
@@ -257,11 +316,19 @@ class PartyLocationViewController: UIViewController, CLLocationManagerDelegate, 
         
     }
     
+    /*!
+     * @discussion Show the party description page
+     */
     func showPartyDescription(){
         print("segue!!")
         self.performSegueWithIdentifier("showPartyDescription", sender: nil)
     }
     
+    /*!
+     * @discussion Set the party's data to the party description screen
+     * @param segue: UIStoryboardSegue
+     * @param sender: AnyObject!
+     */
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
         if (segue.identifier == "showPartyDescription") {
             //get a reference to the destination view controller
@@ -276,7 +343,14 @@ class PartyLocationViewController: UIViewController, CLLocationManagerDelegate, 
     
     @IBOutlet weak var changeDisplay: UIBarButtonItem!
     @IBOutlet weak var party_list: UITableView!
+    
+    // By default, the current display is map
     var currentDisplayStyle = "map"
+    
+    /*!
+     * @discussion Showing the list view for the parties
+     * @param sender: UIBarButtonItem
+     */
     @IBAction func showListView(sender: UIBarButtonItem) {
         print("show list view")
         
@@ -299,14 +373,21 @@ class PartyLocationViewController: UIViewController, CLLocationManagerDelegate, 
         
     }
     
+    // Number of sections in the party list
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
     
+    // Number of rows in the party list
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.filteredParties.count
     }
     
+    /*!
+     * @discussion Rendering t
+     * @param <#param description#>
+     * @return <#return description#>
+     */
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
 
@@ -318,7 +399,11 @@ class PartyLocationViewController: UIViewController, CLLocationManagerDelegate, 
         cell.detailTextLabel?.text = "\(hostInfo!["address"]!)"
         return cell
     }
-    
+    /*!
+     * @discussion <#description#>
+     * @param <#param description#>
+     * @return <#return description#>
+     */
     func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath?{
         self.selectedParty = self.parties[indexPath.row] as? Party
         isPartySelected = true
