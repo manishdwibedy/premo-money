9750beac79209a25ab186ad602441f0659810ec5
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-29 14:10:51 -0700

    Documentation of the menu item screen.

--- Premo/MenuItemViewController.swift
+++ Premo/MenuItemViewController.swift
@@ -10,13 +10,17 @@ import UIKit
 import FirebaseDatabase
 import FirebaseAuth
 
+// This ViewController would be adding new menu items
 class MenuItemViewController: UIViewController {
     @IBOutlet weak var party_type: UISegmentedControl!
     @IBOutlet weak var menu_desc: UITextView!
     @IBOutlet weak var menu_item: UITextField!
     
+    // The DB reference
     let db_ref = FIRDatabase.database().reference()
-    
+
+    // The UID representing the current logged in user
+    let uid = FIRAuth.auth()?.currentUser?.uid
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,14 +29,21 @@ class MenuItemViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    /*!
+     * @discussion Addition of a new menu item
+     * @param sender - add menu button
+     */
     @IBAction func addMenu(sender: AnyObject) {
         print(party_type.selectedSegmentIndex)
         
+        // The party type
         let type = Constant.party_type[party_type.selectedSegmentIndex]
+        
+        // Getting the party's first character
         let index = type.startIndex.advancedBy(1)
         
-        let uid = FIRAuth.auth()?.currentUser?.uid
-        
+        // Getting the user's menu list
         let _ = self.db_ref.child("user_info/\(uid!)/menu").observeSingleEventOfType(FIRDataEventType.Value, withBlock: { (snapshot) in
             let menu_object = snapshot.value
             var menu_array = [[String: AnyObject]]()
@@ -40,9 +51,6 @@ class MenuItemViewController: UIViewController {
             if let dd = menu_object as? [[String: AnyObject]]{
                     menu_array = dd
             }
-//            if let menu_try = menu_object as! [[String : AnyObject]]{
-//                menu_array = menu_try
-//            }
             let menu = [
                 "name": self.menu_item.text!,
                 "description" : self.menu_desc.text!,
@@ -50,13 +58,8 @@ class MenuItemViewController: UIViewController {
             ]
             menu_array.append(menu)
             
-            self.db_ref.child("user_info/\(uid!)/menu").setValue(menu_array)
-
-//            
-//            print(user_data)
-//            self.dob.text = user_data["dob"]!! as? String
-//            self.address.text = user_data["address"]!! as? String
-//            self.username.text = user_data["username"]!! as? String
+            // Adding a menu item
+            self.db_ref.child("user_info/\(self.uid!)/menu").setValue(menu_array)
         })
         
     }
